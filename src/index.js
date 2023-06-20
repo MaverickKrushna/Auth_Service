082d45ae813110e2f63728488e6e990762b30f23
Krushna chandra sahu <krushnachsahu089@gmail.com>
2023-06-20 12:53:50 +0530

    Added get user by id in repo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
 const apiRoutes = require('./routes/index');
-
+const UserRepository = require('./repository/user-repository');
 const app = express();
 
 
@@ -15,9 +15,15 @@ const prepareAndServer=()=>{
 
     app.use('/api' , apiRoutes);
 
-    app.listen(3001,()=>{
+    app.listen(3001,async ()=>{
         console.log('Server started ');
-    }); 
+        // const repo = new UserRepository();  
+        // const response = await repo.getById(1); 
+        // console.log(response);   
+        
+        
+         }); 
+
 }
 
 prepareAndServer(); 
